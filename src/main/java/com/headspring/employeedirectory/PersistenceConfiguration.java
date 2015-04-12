93d1b22dc4e9ac68786bf93fde9321ca66abde5d
Jason Bilyeu <jasonbilyeu@ymail.com>
2015-04-12 08:01:45 -0500

    Adding some properties that drive the number of seed employees to create and urls to allow access header origin from.

--- src/main/java/com/headspring/employeedirectory/PersistenceConfiguration.java
+++ src/main/java/com/headspring/employeedirectory/PersistenceConfiguration.java
@@ -4,6 +4,7 @@ package com.headspring.employeedirectory;
 import com.headspring.employeedirectory.db.Employee;
 import com.headspring.employeedirectory.db.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.jpa.repository.config.JpaRepositoryConfigExtension;
@@ -20,6 +21,9 @@ public class PersistenceConfiguration extends JpaRepositoryConfigExtension {
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    @Value("${numberOfSeedEmployees:10}")
+    private int seedEmployeeCount;
+
     @PostConstruct
     public void insertTestEmployees() {
         employeeRepository.save(Employee.builder()
@@ -29,9 +33,10 @@ public class PersistenceConfiguration extends JpaRepositoryConfigExtension {
                 .firstName("admin")
                 .lastName("user")
                 .jobTitle("HR Specialist")
+                .location("Austin")
                 .phoneNumbers(Collections.singletonList("(512) 555-5555"))
                 .build());
-        for (int i = 0; i < 1; i++) {
+        for (int i = 0; i < seedEmployeeCount; i++) {
             employeeRepository.save(Employee.builder()
                     .email(String.format("user%s@headspring.com", i))
                     .password("password")
@@ -39,6 +44,7 @@ public class PersistenceConfiguration extends JpaRepositoryConfigExtension {
                     .firstName("regular")
                     .lastName(String.format("user%s", i))
                     .jobTitle("Engineer")
+                    .location("Austin")
                     .phoneNumbers(Collections.singletonList("(512) 666-6666"))
                     .build());
         }
--- src/main/java/com/headspring/employeedirectory/SecurityConfiguration.java
+++ src/main/java/com/headspring/employeedirectory/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.headspring.employeedirectory;
 import com.headspring.employeedirectory.security.EmployeeUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -22,7 +23,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().fullyAuthenticated();
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .anyRequest().authenticated();
         http.httpBasic();
         http.csrf().disable();
     }
